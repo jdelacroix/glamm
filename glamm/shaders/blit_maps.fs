4029754ca9fcddd1d3a9e4c8817110fa35d4481d
Jean-Pierre de la Croix <ajpl.delacroix@gmail.com>
2023-06-27 21:03:13 -0700

    Simple color test for merging. Emerald green for overlaps.

--- glamm/shaders/blit_maps.fs
+++ glamm/shaders/blit_maps.fs
@@ -13,17 +13,13 @@ main()
   mediump vec4 map_color = texture(input_texture, target_coordinates);
   mediump vec4 merged_map_color = texture(output_texture, target_coordinates);
 
-  // if (map_color.a == 1.0) {
-  //   if (merged_map_color.a == 1.0) {
-  //     fragment_color = vec4(0.31, 0.78, 0.47, 1.0);
-  //   } else {
-  //     fragment_color = map_color;
-  //   }
-  // }
-
   if (map_color.r != 0.5) {
     //   // fragment_color = vec4(0.0, 0.48, 0.65, 1.0);
-    fragment_color = vec4(map_color.rgb, 1.0);
+    if (merged_map_color.r == 0.5) {
+      fragment_color = vec4(map_color.rgb, 1.0);
+    } else {
+      fragment_color = vec4(0.31, 0.78, 0.47, 1.0); // emerald greend
+    }
   } else {
     fragment_color = vec4(merged_map_color.rgb, 1.0);
   }
